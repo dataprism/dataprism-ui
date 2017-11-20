115da8544481f4639578cb90316d65478a8598fe
Matthias De Vriendt <devriendt.matthias@gmail.com>
2017-11-20 12:09:46 +0100

    menu and color scheme

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,20 @@
 <template>
   <v-app id="inspire" dark>
-    <v-navigation-drawer
-      clipped
-      persistent
-      v-model="drawer"
-      enable-resize-watcher
-      app
-    >
-      <v-list dense>
-        <v-list-tile @click="onNav('Instances')">
-          <v-list-tile-action>
-            <v-icon>list</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Instances</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile @click="onNav('About')">
-          <v-list-tile-action>
-            <v-icon>info_outline</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>About</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
+    <v-toolbar app></v-toolbar>
     <v-toolbar app fixed clipped-left>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Kfunc</v-toolbar-title>
+      <v-icon>change_history</v-icon>
+      <v-toolbar-title>Data Room</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>sync</v-icon>
+      </v-btn>
+      <v-btn icon @click="onNav('Instances')">
+        <v-icon>code</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
     </v-toolbar>
     <main>
       <v-content>
@@ -48,8 +34,6 @@ export default {
   name: 'app',
   data () {
     return {
-      items: ['een', 'twee'],
-      drawer: true
     }
   },
   methods: {

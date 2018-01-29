d5923fab856274dd3fc9e19ce47d799dbce3f2e7
calmera <daan.gerits+code@gmail.com>
2018-01-29 17:35:13 +0100

    minor changes to the sync part

--- src/components/pages/sync/Links.vue
+++ src/components/pages/sync/Links.vue
@@ -1,5 +1,13 @@
 <template>
-  <v-container fluid fill-height>
+  <v-container fluid app class="pa-0" fill-height>
+    <v-layout column>
+      <v-toolbar flat dense color="sync_base">
+        <v-spacer></v-spacer>
+        <v-toolbar-side-icon class="hidden-md-and-up"></v-toolbar-side-icon>
+        <v-toolbar-items class="hidden-sm-and-down">
+        </v-toolbar-items>
+      </v-toolbar>
+      <v-flex class="ma-4" >
         <v-layout v-if="!links.length" justify-center align-center>
           <v-flex xs4>
             <v-card color="secondary" class="white--text">
@@ -9,7 +17,7 @@
               </v-card-title>
               <v-card-actions>
                 <v-btn flat dark @click="search()">Try again</v-btn>
-                <v-btn flat dark @click.native.stop="edit()">Create link</v-btn>
+                <v-btn flat dark @click.native.stop="editing = true">Create link</v-btn>
               </v-card-actions>
             </v-card>
           </v-flex>
@@ -19,7 +27,7 @@
           <v-flex xs12 sm10 offset-sm1>
             <list-item
               v-for="(link, key) in links "
-              v-on:edit="edit(link)"
+              v-on:edit="edit(link.id)"
               type="link"
               :title = "link.name"
               :status = "link.status"
@@ -30,6 +38,9 @@
             ></list-item>
           </v-flex>
         </v-layout>
+      </v-flex>
+    </v-layout>
+
 
     <v-bottom-sheet v-model="editing" inset max-width="70%">
       <v-btn
@@ -38,53 +49,26 @@
         right
         fab
         color="sync_light"
-        @click.native.stop="edit()"
         slot="activator"
       >
         <v-icon>add</v-icon>
       </v-btn>
       <v-card>
         <v-toolbar style="flex: 0 0 auto;" dense flat color="sync_base">
-          <v-toolbar-title>{{ mode === 'edit' ? 'Edit' : 'Create'}} Link</v-toolbar-title>
+          <v-toolbar-title>Create Link</v-toolbar-title>
         </v-toolbar>
         <v-card-text>
           <v-form>
             <v-text-field
               label="Id"
-              v-model="link.id"
-              required
-              :disabled="mode === 'edit'"
-            ></v-text-field>
-            <v-text-field
-              label="Name"
-              v-model="link.name"
+              v-model="linkId"
               required
             ></v-text-field>
-            <v-select
-              :items="connectors"
-              v-model="link.connector_id"
-              label="Connector"
-              dark
-              item-value="id"
-              item-text="name"
-            ></v-select>
-            <span class="subheader">settings</span>
-            <v-text-field
-              :label="prop.key"
-              v-model="link.settings[prop.key]"
-              v-for="prop in connector.properties"
-            ></v-text-field>
-            <!--<editable-grid :value="link.settings" idField="key" titleField="key" descriptionField="description" ></editable-grid>-->
           </v-form>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn v-if="mode === 'create'" class="sync_base" @click="create()">
-            create
-          </v-btn>
-          <v-btn v-if="mode === 'edit'" class="sync_base" @click="update()">
-            save
-          </v-btn>
+          <v-btn class="sync_base" @click="create()">create</v-btn>
         </v-card-actions>
       </v-card>
 
@@ -101,51 +85,28 @@
     name: 'Links',
     computed: {
       ...mapGetters({
-        links: 'links/links',
-        connector: 'links/connector',
-        connectors: 'connectors/connectors'
-      }),
-      editing: {
-        // eslint-disable-next-line
-        get: function () {
-          return this.$store.getters['links/editing']
-        },
-        // eslint-disable-next-line
-        set: function (v) {
-          if (!v) return this.$store.dispatch('links/STOP_EDITING')
-
-          return this.$store.dispatch('links/START_EDITING')
-        }
-      }
+        links: 'links/links'
+      })
     },
     data () {
       return {
-        mode: '',
-        link: {
-          settings: {}
-        }
+        linkId: '',
+        editing: false
       }
     },
     methods: {
-      edit (link) {
-        this.link = link ? JSON.parse(JSON.stringify(link)) : { properties: [] }
-        this.mode = link ? 'edit' : 'create'
-
-        this.$store.dispatch('links/START_EDITING', link)
+      edit (linkId) {
+        this.$router.push({ name: 'Link', params: { id: linkId } })
       },
       create () {
-        this.$store.dispatch('links/CREATE', this.link)
-      },
-      update () {
-        this.$store.dispatch('links/UPDATE', this.link)
+        this.$store.dispatch('links/CREATE', this.linkId)
       },
       ...mapActions({
-        search: 'links/SEARCH'
+        search: 'links/LIST_LINKS'
       })
     },
     created () {
-      this.$store.dispatch('links/SEARCH')
-      this.$store.dispatch('connectors/SEARCH')
+      this.$store.dispatch('links/LIST_LINKS')
     }
   }
 </script>
